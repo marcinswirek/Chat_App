df09ec1ce03ff4e6cf0deef0e1710c085dab2c44
Marcin Świrek <swirek.marcin@gmail.com>
2018-06-07 00:42:06 +0200

    Change app container from root to app in client/index.js

--- client/index.js
+++ client/index.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
+import React from "react";
+import ReactDOM from "react-dom";
+import { AppContainer } from "react-hot-loader";
 
-import App from './App';
+import App from "./App";
 
-const render = (Component) => {
-    ReactDOM.render(
-        <AppContainer>
-            <Component/>
-        </AppContainer>,
-        document.getElementById('root')
-    );
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById("app")
+  );
 };
 
 render(App);
 
 if (module.hot) {
-    module.hot.accept('./App', () => {
-        const NewApp = require('./App').default;
-        render(NewApp)
-    });
+  module.hot.accept("./App", () => {
+    const NewApp = require("./App").default;
+    render(NewApp);
+  });
 }
